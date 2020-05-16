9ab0e9b2ae00bfba1af573827feb3672f4873424
JairZijp <jairzijp123@gmail.com>
2020-05-16 12:06:46 +0200

    config & structure

--- app/src/main/java/com/example/hsdeckmaker/ui/MainActivity.kt
+++ app/src/main/java/com/example/hsdeckmaker/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hsdeckmaker
+package com.example.hsdeckmaker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hsdeckmaker.R
 
 class MainActivity : AppCompatActivity() {
 
